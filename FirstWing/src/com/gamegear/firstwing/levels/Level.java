bfa47689ba43b15ab37b1002b560fe96496c7a4f
John <johnnyvdberg@gmail.com>
2013-09-19 16:09:15 +0200

    Fixed some issues with controls User can touch selected props on screen

--- FirstWing/src/com/gamegear/firstwing/levels/Level.java
+++ FirstWing/src/com/gamegear/firstwing/levels/Level.java
@@ -1,8 +1,6 @@
 package com.gamegear.firstwing.levels;
 
 import java.util.Iterator;
-import java.util.Map;
-import java.util.Map.Entry;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.math.Vector2;
