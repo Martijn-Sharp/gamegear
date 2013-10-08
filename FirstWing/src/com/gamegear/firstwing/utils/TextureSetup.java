dedeb52f2d564687bd8337d11dbc6597d3c5a3f5
John <johnnyvdberg@gmail.com>
2013-10-08 21:14:13 +0200

    Fixed some stuff

--- FirstWing/src/com/gamegear/firstwing/utils/TextureSetup.java
+++ FirstWing/src/com/gamegear/firstwing/utils/TextureSetup.java
@@ -6,9 +6,10 @@ public class TextureSetup {
 	public static void main(String[] args) {
 //		TexturePacker2.process("D:/School/Minor/Local/FirstWing-images/dynamic", "D:/School/Minor/Local/FirstWing-android/assets/textures", "dyntextures.atlas");
 //		TexturePacker2.process("D:/School/Minor/Local/FirstWing-images/static", "D:/School/Minor/Local/FirstWing-android/assets/textures", "statextures.atlas");
+		TexturePacker2.process("D:/School/Minor/Local/FirstWing-android/assets/ui/processing", "D:/School/Minor/Local/FirstWing-android/assets/ui", "ui.atlas");
 		
-		TexturePacker2.process("D:/FirstWing/FirstWing-images/dynamic", "D:/FirstWing/FirstWing-android/assets/textures", "dyntextures.atlas");
-		TexturePacker2.process("D:/FirstWing/FirstWing-images/static", "D:/FirstWing/FirstWing-android/assets/textures", "statextures.atlas");
-		TexturePacker2.process("D:/FirstWing/FirstWing-images/ui", "D:/FirstWing/FirstWing-android/assets/ui", "uiskin.atlas");
+//		TexturePacker2.process("D:/FirstWing/gamegear/FirstWing-images/dynamic", "D:/FirstWing/gamegear/FirstWing-android/assets/textures", "dyntextures.atlas");
+//		TexturePacker2.process("D:/FirstWing/gamegear/FirstWing-images/static", "D:/FirstWing/gamegear/FirstWing-android/assets/textures", "statextures.atlas");
+//		TexturePacker2.process("D:/FirstWing/gamegear/FirstWing-images/ui", "D:/FirstWing/gamegear/FirstWing-android/assets/ui", "uiskin.atlas");
 	}
 }
